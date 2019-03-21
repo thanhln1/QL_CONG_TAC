c7bb330b731796ca42676277c55c056d392f1381
thanhln1 <thanhln1@runsystem.net>
2019-03-21 21:23:08 +0700

    update

--- DAO/MT_DON_GIA_DAO.cs
+++ DAO/MT_DON_GIA_DAO.cs
@@ -27,6 +27,44 @@ namespace DAO
                 }
             }
         }
+
+        public List<MT_DON_GIA> getAllDonGia()
+        {
+            using (IDbConnection cnn = new System.Data.SqlClient.SqlConnection(dao.ConnectionString("Default")))
+            {
+                var output = cnn.Query<MT_DON_GIA>("select * from MT_DON_GIA ").ToList();
+                if (output.Count > 0)
+                {
+                    return output.ToList();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public bool UpdateDonGia(MT_DON_GIA dongia)
+        {
+            try
+            {
+                using (IDbConnection cnn = new System.Data.SqlClient.SqlConnection(dao.ConnectionString("Default")))
+                {
+                    StringBuilder sql = new StringBuilder();
+                    sql.Append("UPDATE MT_DON_GIA set ");
+                    sql.Append("DON_GIA=@DON_GIA, ");
+                    sql.Append("GHI_CHU=@GHI_CHU ");
+                    sql.Append(" WHERE ID = @ID; ");
+
+                    cnn.Execute(sql.ToString(), dongia);
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
    
 }
